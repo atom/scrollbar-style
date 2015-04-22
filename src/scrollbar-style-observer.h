d73961e2e95dd620b29ef760bc2bc27e611da88a
Kevin Sawicki <kevinsawicki@gmail.com>
2015-04-22 10:11:15 -0700

    Add cpplint

--- src/scrollbar-style-observer.h
+++ src/scrollbar-style-observer.h
@@ -1,5 +1,26 @@
-#ifndef SRC_SCROLLER_STYLE_OBSERVER_H_
-#define SRC_SCROLLER_STYLE_OBSERVER_H_
+// Copyright (c) 2015 GitHub Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#ifndef SRC_SCROLLBAR_STYLE_OBSERVER_H_
+#define SRC_SCROLLBAR_STYLE_OBSERVER_H_
 
 #include "nan.h"
 
@@ -11,7 +32,7 @@ class ScrollbarStyleObserver : public node::ObjectWrap {
   void HandleScrollbarStyleChanged();
 
  private:
-  ScrollbarStyleObserver(NanCallback *callback);
+  explicit ScrollbarStyleObserver(NanCallback *callback);
   ~ScrollbarStyleObserver();
   static NAN_METHOD(New);
   static NAN_METHOD(GetPreferredScrollbarStyle);
@@ -19,4 +40,4 @@ class ScrollbarStyleObserver : public node::ObjectWrap {
   NanCallback *callback;
 };
 
-#endif  // SRC_SCROLLER_STYLE_OBSERVER_H_
+#endif  // SRC_SCROLLBAR_STYLE_OBSERVER_H_
